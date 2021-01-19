7821c47a7249a35eb124244b8251cb499cb59053
Victor Chang <vichang@google.com>
2021-01-19 10:18:02 +0000

    Android patch: Allow absolute paths for ctest's -x.
    ctest incorrectly identifies absolute paths to -x as a test filter.
    Skip the argument after -x when finding tese filters so absolute paths
    are usable.
    
    The upstream request is tracked at
    https://unicode-org.atlassian.net/browse/ICU-13849.
    
    Bug: 120776993
    Test: treehugger
    Change-Id: Ib1ae5bdb71e82498c292872e09b79649f0bb5ac4

--- icu4c/source/tools/ctestfw/ctest.c
+++ icu4c/source/tools/ctestfw/ctest.c
@@ -1117,6 +1117,7 @@ runTestRequest(const TestNode* root,
     int                i;
     int                doList = false;
     int                subtreeOptionSeen = false;
+    int                skipNext = false;
 
     int                errorCount = 0;
 
@@ -1128,6 +1129,11 @@ runTestRequest(const TestNode* root,
 
     for( i=1; i<argc; i++)
     {
+        if (skipNext) {
+            skipNext = false;
+            continue;
+        }
+
         if ( argv[i][0] == '/' )
         {
             printf("Selecting subtree '%s'\n", argv[i]);
@@ -1159,6 +1165,10 @@ runTestRequest(const TestNode* root,
             subtreeOptionSeen=false;
         } else if (strcmp( argv[i], "-l") == 0) {
             doList = true;
+       } else if (strcmp( argv[i], "-x") == 0) {
+            // Need to skip the next argument since it will be wrongly
+            // identified as a test filter if it is an absolute path.
+            skipNext = true;
         }
         /* else option already handled by initArgs */
     }
